e6722c48eea9ef6ac1da9067793a5e02bc4fc29c
akashbn <akash.bn@live.com>
2018-10-31 18:42:50 +0530

    Added Rental list, details, service and model

---
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Rental } from '../shared/rental.model';
+import { RentalService } from '../shared/rental.service';
+
+@Component({
+  selector: 'bwm-rental-detail',
+  templateUrl: './rental-detail.component.html',
+  styleUrls: ['./rental-detail.component.scss']
+})
+export class RentalDetailComponent implements OnInit {
+  rental: Rental;
+  constructor(private route: ActivatedRoute, private rentalService: RentalService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (param) => {
+        this.getRental(param['rentalId']);
+      }
+    );
+  }
+
+  getRental(rentalId: string) {
+    this.rentalService.getRentalById(rentalId).subscribe(
+      (rental: Rental) => {
+        this.rental = rental;
+      }
+    );
+  }
+
+}
